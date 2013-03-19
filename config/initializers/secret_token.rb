5fb80e7acb932d898ebadbe387c56cfbcf9cc2d8
Dan Ryan <scriptfu@gmail.com>
2013-03-19 03:09:30 -0400

    c'mon, rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Reveille::Application.config.secret_token = Figaro.env.secret_token
+
+Reveille::Application.config.secret_token = (Rails.env.test? ? "foooooo" : Figaro.env.secret_token)
