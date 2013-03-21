936d7536350f32adeff6c046b28b12ca321b46b8
Dan Ryan <scriptfu@gmail.com>
2013-03-21 15:10:30 -0400

    new hash syntax

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,6 +1,6 @@
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
-  config.include Devise::TestHelpers, :type => :request
+  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :request
 
   config.include Warden::Test::Helpers, type: :request
   config.include Warden::Test::Helpers, type: :controller
