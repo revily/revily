99992e0280a8347ccd77056a3622b9601c7bcd39
Dan Ryan <scriptfu@gmail.com>
2013-05-09 12:28:29 -0400

    nicer flash messages

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -8,16 +8,22 @@ module BootstrapFlashHelper
       next if message.blank?
 
       type = :success if type == :notice
-      type = :error   if type == :alert
+      type = :danger   if type == :alert
       next unless ALERT_TYPES.include?(type)
 
+      # Array(message).each do |msg|
+      #   text = content_tag :div, class: 'row-fluid' do
+      #     content_tag :div, class: 'span12' do
+      #       content_tag :div, :class => "alert fade in alert-#{type}" do
+      #         content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") + msg.html_safe
+      #       end
+      #     end
+      #   end
+      #   flash_messages << text if message
+      # end
       Array(message).each do |msg|
-        text = content_tag :div, class: 'row-fluid' do
-          content_tag :div, class: 'span12' do
-            content_tag :div, :class => "alert fade in alert-#{type}" do
-              content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") + msg.html_safe
-            end
-          end
+        text = content_tag :div, class: "dialog dialog-#{type} fade in" do
+          content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") + msg.html_safe
         end
         flash_messages << text if message
       end
