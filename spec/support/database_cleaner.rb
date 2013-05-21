4174b8585c33062eba52b17f51b44561726c0d2f
Dan Ryan <scriptfu@gmail.com>
2013-05-21 00:45:03 -0400

    use truncation strategy to clean test db

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -28,8 +28,7 @@ require 'database_cleaner'
 
 RSpec.configure do |config|
   config.before(:suite) do
-
-    DatabaseCleaner.clean_with(:transaction)
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
