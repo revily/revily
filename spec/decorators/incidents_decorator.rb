a386442c83420e1661e016e96e27c35ebb53bf58
Dan Ryan <scriptfu@gmail.com>
2013-07-13 01:36:21 -0400

    fix the build

--- spec/decorators/incidents_decorator.rb
+++
@@ -1,3 +0,0 @@
-class IncidentsDecorator < Draper::CollectionDecorator
-  delegate :triggered, :acknowledged, :resolved, :unresolved
-end
--- spec/decorators/service_decorator_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-
-describe ServiceDecorator do
-end
