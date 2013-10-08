922c11333acacc88ae47a497e8b7d3c9fd9e174d
Dan Ryan <scriptfu@gmail.com>
2013-10-08 13:21:30 -0400

    sessions controller for clients

--- app/controllers/v1/users/sessions_controller.rb
+++ app/controllers/v1/users/sessions_controller.rb
@@ -1,44 +1,92 @@
 class V1::Users::SessionsController < Devise::SessionsController
+  before_filter :authenticate_user!, except: [ :create ]
+  before_filter :ensure_params_exist, except: [ :destroy ]
+  respond_to :json
+
   def create
-    return missing_params unless params[:email] && params[:password]
+    resource = User.find_for_database_authentication(email: session_params[:email])
+    return invalid_login_attempt unless resource
 
-    build_resource
-    resource = resource_from_credentials
-    return invalid_credentials unless resource
-
-    resource.ensure_authentication_token!
-    data = {
-      # user: {
-        user_id: resource.id,
-        auth_token: resource.authentication_token
-      # }
-    }
-
-    render json: data, status: 201
+    if resource.valid_password?(session_params[:password])
+      sign_in(:user, resource)
+      resource.ensure_authentication_token!
+      render json: { id: resource.uuid, auth_token: resource.authentication_token }, status: :ok
+      return
+    end
+    invalid_login_attempt
   end
 
   def destroy
-    return missing_params unless params[:auth_token]
-
-    resource = resource_class.find_by_authentication_token(params[:auth_token])
-    return invalid_credentials unless resource
-
+    resource = User.find_by_authentication_token(params[:auth_token] || header_token)
     sign_out(resource_name)
-    # resource.reset_authentication_token!
-
-    render json: { user_id: resource.id }, status: 200
+    render json: {}.to_json, status: :ok
   end
 
   protected
 
-  def resource_from_credentials
-    data = { email: params[:email] }
-    if res = resource_class.find_for_database_authentication(data)
-      if res.valid_password?(params[:password])
-        res
-      end
+  def session_params
+    params.permit(:email, :password)
+  end
+  
+  def ensure_params_exist
+    return unless session_params[:email].blank? && session_params[:password].blank?
+    render json: { message: "email and password are required parameters" }, status: 422
+  end
+
+  def invalid_login_attempt
+    render json: { message: "there was an error with your email or password" }, status: 401
+  end
+
+  def header_token
+    header_token = request.headers["Authorization"]
+    if header_token && header_token =~ /^token (.+)$/
+      $~[1]
     end
   end
+
+
+  # def create
+  #   return missing_params unless params[:email] && params[:password]
+
+  #   build_resource
+  #   resource = resource_from_credentials
+  #   return invalid_credentials unless resource
+
+  #   resource.ensure_authentication_token!
+  #   data = {
+  #     # user: {
+  #       user_id: resource.id,
+  #       auth_token: resource.authentication_token
+  #     # }
+  #   }
+
+  #   render json: data, status: 201
+  # end
+
+  # def destroy
+  #   return missing_params unless params[:auth_token]
+
+  #   resource = resource_class.find_by_authentication_token(params[:auth_token])
+  #   return invalid_credentials unless resource
+
+  #   sign_out(resource_name)
+  #   # resource.reset_authentication_token!
+
+  #   render json: { user_id: resource.id }, status: 200
+  # end
+
+  # protected
+
+
+
+  # def resource_from_credentials
+  #   data = { email: params[:email] }
+  #   if res = resource_class.find_for_database_authentication(data)
+  #     if res.valid_password?(params[:password])
+  #       res
+  #     end
+  #   end
+  # end
 
   def missing_params
     warden.custom_failure!
