656c1983f0a3c8022b4732dbd0c7db4950929832
Dan Ryan <scriptfu@gmail.com>
2013-11-09 12:12:55 -0500

    remove dead code

--- lib/revily/helpers/unique_token.rb
+++ lib/revily/helpers/unique_token.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/string/filters"
+require "securerandom"
 
 module Revily
   module Helpers
--- spec/lib/revily/helpers/unique_token_spec.rb
+++ spec/lib/revily/helpers/unique_token_spec.rb
@@ -1,4 +1,4 @@
-require "spec_helper"
+require "unit_helper"
 
 module Revily::Helpers
   describe UniqueToken do
@@ -13,25 +13,25 @@ module Revily::Helpers
       it "generates a hex token" do
         token = UniqueToken.generate(type: :hex)
         expect(token.length).to eq 64
-        expect(token).to match(/\b[a-f0-9]+\b/)
+        expect(token).to match(/\b[a-f0-9]{64}\b/)
       end
 
       it "generates a custom length hex token" do
         token = UniqueToken.generate(type: :hex, length: 32)
         expect(token.length).to eq 32
-        expect(token).to match(/\b[a-f0-9]+\b/)
+        expect(token).to match(/\b[a-f0-9]{32}\b/)
       end
 
       it "generates a base64 token" do
         token = UniqueToken.generate
         expect(token.length).to eq 64
-        expect(token).to match(/\b[a-zA-Z0-9]+\b/)
+        expect(token).to match(/\b[a-zA-Z0-9]{64}\b/)
       end
 
       it "generates a custom length base64 token" do
         token = UniqueToken.generate(length: 8)
         expect(token.length).to eq 8
-        expect(token).to match(/\b[a-zA-Z0-9]+\b/)
+        expect(token).to match(/\b[a-zA-Z0-9]{8}\b/)
       end
 
     end
